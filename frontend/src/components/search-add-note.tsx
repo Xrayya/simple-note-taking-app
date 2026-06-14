992b0e532f85b2853ec0e137ecfa44f811574b88
Xrayya <nur.alkhiyar@gmail.com>
2026-06-14 08:45:36 +0700

    feat: add search bar and add note

---
+++ frontend/src/components/search-add-note.tsx
@@ -0,0 +1,37 @@
+import { SearchIcon, X, NotebookPen } from "lucide-react";
+import { Button } from "./ui/button";
+import { Field, FieldGroup, FieldSet } from "./ui/field";
+import {
+  InputGroup,
+  InputGroupInput,
+  InputGroupAddon,
+  InputGroupButton,
+} from "./ui/input-group";
+
+export function SearchAddNote() {
+  return (
+    <FieldSet className="w-full my-4">
+      <FieldGroup className="flex flex-row gap-4">
+        <Field>
+          <InputGroup>
+            <InputGroupInput id="search-note" placeholder="Search Note..." />
+            <InputGroupAddon align="inline-start">
+              <SearchIcon />
+            </InputGroupAddon>
+            <InputGroupAddon align="inline-end">12 results</InputGroupAddon>
+            <InputGroupAddon align="inline-end">
+              <InputGroupButton variant="secondary" type="button">
+                <X />
+              </InputGroupButton>
+            </InputGroupAddon>
+          </InputGroup>
+        </Field>
+        <Field className="w-fit">
+          <Button type="button">
+            <NotebookPen /> New Note
+          </Button>
+        </Field>
+      </FieldGroup>
+    </FieldSet>
+  );
+}
