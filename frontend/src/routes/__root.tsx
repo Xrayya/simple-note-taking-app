a6804525a751bafbeaca7a4dd8050d5d6d236ca4
Xrayya <nur.alkhiyar@gmail.com>
2026-06-14 08:37:49 +0700

    feat: set container, margins, paddings

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,28 +1,30 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { TanStackDevtools } from "@tanstack/react-devtools";
 
-import '../styles.css'
+import "../styles.css";
 
 export const Route = createRootRoute({
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <>
-      <Outlet />
+      <div className="container mx-auto px-8 lg:px-32">
+        <Outlet />
+      </div>
       <TanStackDevtools
         config={{
-          position: 'bottom-right',
+          position: "bottom-right",
         }}
         plugins={[
           {
-            name: 'TanStack Router',
+            name: "TanStack Router",
             render: <TanStackRouterDevtoolsPanel />,
           },
         ]}
       />
     </>
-  )
+  );
 }
