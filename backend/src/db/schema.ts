5eefe0a3fdb88ff5e92fe7157dfed624b748363b
Xrayya <nur.alkhiyar@gmail.com>
2026-06-17 14:34:21 +0700

    feat(be.db.schema): add timestamp and make isArchived not null

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,10 +1,16 @@
-import { boolean, pgTable, text, uuid } from "drizzle-orm/pg-core";
+import { boolean, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 const NOTE_TABLE_NAME = `${process.env.DATABASE_TABLE_PREFIX!}-user`;
+
+const timestamps = {
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at"),
+};
 
 export const notes = pgTable(NOTE_TABLE_NAME, {
   id: uuid("id").primaryKey().defaultRandom(),
   title: text("title").notNull(),
   body: text("body").notNull(),
-  isArchived: boolean("arhived").default(false),
+  isArchived: boolean("arhived").default(false).notNull(),
+  ...timestamps
 });
