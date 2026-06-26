99f693550d347a23030f239d041fce4856b166eb
Xrayya <nur.alkhiyar@gmail.com>
2026-06-26 12:50:34 +0700

    feat(fe): integrate archive/unarchive and delete note

--- frontend/src/components/note-card.tsx
+++ frontend/src/components/note-card.tsx
@@ -8,8 +8,13 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import type { Note } from "@/contexts/notes-context";
-import { useNotes } from "@/hooks/use-notes";
 import { cn } from "@/lib/utils";
+import {
+  queryOptions,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { LoaderCircle } from "lucide-react";
 import type { ComponentProps } from "react";
 
 type Props = ComponentProps<"div"> & Note;
@@ -22,10 +27,85 @@ export function NoteCard({
   className,
   ...restProps
 }: Props) {
-  const { updateNote, deleteNote } = useNotes();
+  const queryClient = useQueryClient();
+
+  const archiveUnarchiveNote = useMutation({
+    mutationFn: async (): Promise<{
+      newNote: {
+        id: string;
+        createdAt: Date;
+        updatedAt: Date | null;
+        title: string;
+        body: string;
+        isArchived?: boolean;
+      };
+    }> => {
+      const url = new URL(`/notes/${id}`, import.meta.env.VITE_BACKEND_ENDPOINT);
+
+      const response = await fetch(url, {
+        headers: { "Content-Type": "application/json" },
+        method: "PUT",
+        body: JSON.stringify({ isArchived: !isArchived }),
+      });
+
+      if (!response.ok) {
+        const payload = await response.json();
+
+        throw new Error(
+          payload?.error?.message || "An error occurred while fetching data",
+          { cause: payload?.error?.name },
+        );
+      }
+
+      const payload = await response.json();
+      return payload;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(
+        queryOptions({
+          queryKey: ["notes"],
+        }),
+      );
+    },
+  });
+
+  const deleteNote = useMutation({
+    mutationFn: async (): Promise<{
+      newNote: {
+        id: string;
+        title: string;
+      };
+    }> => {
+      const url = new URL(`/notes/${id}`, import.meta.env.VITE_BACKEND_ENDPOINT);
+
+      const response = await fetch(url, {
+        headers: { "Content-Type": "application/json" },
+        method: "DELETE",
+      });
+
+      if (!response.ok) {
+        const payload = await response.json();
+
+        throw new Error(
+          payload?.error?.message || "An error occurred while fetching data",
+          { cause: payload?.error?.name },
+        );
+      }
+
+      const payload = await response.json();
+      return payload;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(
+        queryOptions({
+          queryKey: ["notes"],
+        }),
+      );
+    },
+  });
 
   return (
-    <Card size="sm" className={cn("mx-auto w-full", className)} {...restProps}>
+    <Card className={cn("mx-auto w-full", className)} {...restProps}>
       <CardHeader>
         <CardTitle>{title}</CardTitle>
         <CardDescription>
@@ -38,26 +118,28 @@ export function NoteCard({
       <CardFooter className="grid grid-cols-2 gap-2">
         <Button
           variant="outline"
-          size="sm"
           className="w-full"
+          disabled={archiveUnarchiveNote.isPending}
           onClick={() => {
-            updateNote(id, {
-              title,
-              body,
-              isArchived: !isArchived,
-            });
+            archiveUnarchiveNote.mutate();
           }}
         >
+          {archiveUnarchiveNote.isPending ? (
+            <LoaderCircle className="animate-spin" />
+          ) : null}
           {isArchived ? "Unarchived" : "Archive"}
         </Button>
         <Button
-          variant="outline"
-          size="sm"
+          variant="destructive"
           className="w-full"
+          disabled={deleteNote.isPending}
           onClick={() => {
-            deleteNote(id);
+            deleteNote.mutate();
           }}
         >
+          {deleteNote.isPending ? (
+            <LoaderCircle className="animate-spin" />
+          ) : null}
           Delete
         </Button>
       </CardFooter>
