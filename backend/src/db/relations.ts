8f6455d9ceb14f5f717df17056c0981e5f279b22
Xrayya <nur.alkhiyar@gmail.com>
2026-07-08 13:45:01 +0700

    feat(be.db): add user and refresh token table and all relations

--- backend/src/db/relations.ts
+++ backend/src/db/relations.ts
@@ -1,4 +1,27 @@
 import { defineRelations } from "drizzle-orm";
 import * as schema from "./schema";
 
-export const relations = defineRelations(schema, (r) => ({}));
+export const relations = defineRelations(schema, (r) => ({
+  users: {
+    notes: r.many.notes({
+      from: r.users.id,
+      to: r.notes.authorId,
+    }),
+    refreshTokens: r.many.refreshTokens({
+      from: r.users.id,
+      to: r.refreshTokens.ownerId,
+    }),
+  },
+  notes: {
+    author: r.one.users({
+      from: r.notes.authorId,
+      to: r.users.id,
+    }),
+  },
+  refreshTokens: {
+    owner: r.one.users({
+      from: r.refreshTokens.ownerId,
+      to: r.users.id,
+    }),
+  },
+}));
--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,16 +1,45 @@
-import { boolean, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import {
+  boolean,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
 
+const USER_TABLE_NAME = `${process.env["DATABASE_TABLE_PREFIX"]!}-users`;
 const NOTE_TABLE_NAME = `${process.env["DATABASE_TABLE_PREFIX"]!}-notes`;
+const REFRESH_TOKEN_TABLE_NAME = `${process.env["DATABASE_TABLE_PREFIX"]!}-refresh-token`;
 
 const timestamps = {
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at"),
 };
 
+export const users = pgTable(USER_TABLE_NAME, {
+  id: uuid("id").primaryKey().defaultRandom(),
+  username: varchar("username", { length: 100 }).notNull(),
+  email: varchar("email", { length: 100 }).notNull().unique(),
+  passwordHash: text("password_hash").notNull(),
+  ...timestamps,
+});
+
 export const notes = pgTable(NOTE_TABLE_NAME, {
   id: uuid("id").primaryKey().defaultRandom(),
   title: text("title").notNull(),
   body: text("body").notNull(),
   isArchived: boolean("archived").default(false).notNull(),
+  authorId: uuid("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
   ...timestamps,
+});
+
+export const refreshTokens = pgTable(REFRESH_TOKEN_TABLE_NAME, {
+  id: uuid("id").defaultRandom().primaryKey(),
+  ownerId: uuid("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  token: uuid("token").notNull().unique().defaultRandom(),
+  expiredAt: timestamp("expired_at").notNull(),
 });
