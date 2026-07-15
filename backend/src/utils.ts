09a39f9caec223d5441801ef91af29c40c5cd84d
Xrayya <nur.alkhiyar@gmail.com>
2026-07-15 11:00:00 +0700

    feat(be): create utils wrapper for bcrypt and jose

---
+++ backend/src/utils.ts
@@ -0,0 +1,33 @@
+import { compareSync, hashSync } from "bcrypt-ts";
+import { jwtVerify, SignJWT, type JWTPayload } from "jose";
+import { InvalidTokenError } from "./exceptions/auth";
+
+export const SECRET = new TextEncoder().encode(process.env["SECRET_KEY"]!);
+
+export const hasher = {
+  encrypt: (data: string): string => {
+    return hashSync(data, 14);
+  },
+  verify: (encryptedPassword: string, inputPassword: string): boolean => {
+    return compareSync(inputPassword, encryptedPassword);
+  },
+};
+
+export const jwt = {
+  sign: async (payload: any): Promise<string> => {
+    return await new SignJWT(payload)
+      .setProtectedHeader({ alg: "HS256" })
+      .setIssuedAt()
+      // .setExpirationTime("10m")
+      .setExpirationTime("4s") // 4 seconds for testing purposes
+      .sign(SECRET);
+  },
+  verify: async (token: string): Promise<JWTPayload> => {
+    try {
+      const { payload } = await jwtVerify(token, SECRET);
+      return payload;
+    } catch (error) {
+      throw new InvalidTokenError();
+    }
+  },
+};
