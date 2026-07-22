356a76613dfa0ce76c9ded47f1f6d2fea7d7e5f6
Xrayya <nur.alkhiyar@gmail.com>
2026-07-22 19:31:39 +0700

    feat(be): add auth middleware

---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,32 @@
+import { createMiddleware } from "hono/factory";
+import type { JWTPayload } from "jose";
+import { AuthenticationRequiredError } from "../exceptions/auth";
+import { jwt } from "../utils";
+
+export const authMiddleware = createMiddleware<{
+  Variables: {
+    user: JWTPayload & {
+      userId: string;
+      username: string;
+      email: string;
+    };
+  };
+}>(async (c, next) => {
+  const token = c.req.header("Authorization")?.split(" ")[1];
+
+  if (!token) {
+    throw new AuthenticationRequiredError();
+  }
+
+  const { payload } = await jwt.verify(token);
+
+  c.set(
+    "user",
+    payload as JWTPayload & {
+      userId: string;
+      username: string;
+      email: string;
+    },
+  );
+  await next();
+});
