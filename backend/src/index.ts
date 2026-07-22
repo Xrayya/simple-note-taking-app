603a7bad8c40535792d4de8f8c2e58d322dd8c20
Xrayya <nur.alkhiyar@gmail.com>
2026-07-22 16:10:00 +0700

    feat(be.index): add auth route and cors config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,10 +4,31 @@ import { logger } from "hono/logger";
 import { BaseError } from "./exceptions/base";
 import { InvalidRequestError } from "./exceptions/validation";
 import { notesRoute } from "./routes/notes";
+import { authRoute } from "./routes/auth";
+import { cors } from "hono/cors";
+import { env, IS_PROD } from "./env";
 
 const app = new Hono();
 
 app.use(logger());
+
+app.use(
+  "*",
+  cors({
+    origin: (origin) => {
+      if (!origin) return !IS_PROD ? "*" : null;
+
+      if (env.ALLOWED_ORIGINS.includes(origin)) {
+        return origin;
+      }
+
+      return null;
+    },
+    credentials: true,
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    allowHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.get("/", (c) => {
   return c.json(
@@ -18,6 +39,7 @@ app.get("/", (c) => {
   );
 });
 
+app.route("/auth", authRoute);
 app.route("/notes", notesRoute);
 
 app.onError((err, c) => {
