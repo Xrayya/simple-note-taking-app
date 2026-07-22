110cb24f9db071fa005891504bb96d803dafa2c2
Xrayya <nur.alkhiyar@gmail.com>
2026-07-22 16:09:40 +0700

    feat(be.env): add `ALLOWED_ORIGINS`

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -7,6 +7,10 @@ const envSchema = z.object({
   DATABASE_URL: z.url().startsWith("postgresql://"),
   DATABASE_TABLE_PREFIX: z.string().default(""),
   JWT_SECRET: z.string().min(8),
+  ALLOWED_ORIGINS: z
+    .string()
+    .default("http://localhost:3001")
+    .transform((origins) => origins.split(",").map((origin) => origin.trim())),
 });
 
 export const env = envSchema.parse(Bun.env);
