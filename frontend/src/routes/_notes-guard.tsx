9e13bad3b45db2d8a79e6059b2b29e0aba0ec8da
Xrayya <nur.alkhiyar@gmail.com>
2026-07-29 07:33:55 +0700

    feat(fe.note-guard): insert auth guard

--- frontend/src/routes/_notes-guard.tsx
+++ frontend/src/routes/_notes-guard.tsx
@@ -16,11 +16,21 @@ import {
   useSidebar,
 } from "#/components/ui/sidebar.tsx";
 import { useIsMobile } from "#/hooks/use-mobile.ts";
+import { authMeOption } from "#/lib/api.ts";
 import { cn } from "#/lib/utils.ts";
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Navigate, Outlet } from "@tanstack/react-router";
 import { useState } from "react";
+import { Route as loginRoute } from "./login.tsx";
 
 export const Route = createFileRoute("/_notes-guard")({
+  beforeLoad: async ({ context }) => {
+    try {
+      const data = await context.queryClient.fetchQuery(authMeOption);
+      return { user: data };
+    } catch {
+      return { user: null };
+    }
+  },
   component: RouteComponent,
 });
 
@@ -84,6 +94,12 @@ function RouteLayout() {
 }
 
 function RouteComponent() {
+  const { user } = Route.useRouteContext();
+
+  if (!user) {
+    return <Navigate to={loginRoute.to} />;
+  }
+
   return (
     <SidebarProvider>
       <RouteLayout />
