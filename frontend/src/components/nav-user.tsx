20902867ad79adaaf6a663afa383c27f1bcd6bba
Xrayya <nur.alkhiyar@gmail.com>
2026-08-05 06:06:19 +0700

    feat(fe.sidebar): properly show user info

--- frontend/src/components/nav-user.tsx
+++ frontend/src/components/nav-user.tsx
@@ -1,3 +1,4 @@
+import { authMeOption } from "#/lib/api.ts";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
@@ -14,6 +15,7 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { useQueryClient } from "@tanstack/react-query";
 import {
   BadgeCheckIcon,
   BellIcon,
@@ -33,6 +35,9 @@ export function NavUser({
   };
 }) {
   const { isMobile } = useSidebar();
+  const queryClient = useQueryClient();
+
+  const userData = queryClient.getQueryData(authMeOption.queryKey);
 
   return (
     <SidebarMenu>
@@ -44,12 +49,19 @@ export function NavUser({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="size-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
+                <AvatarImage
+                  src={user.avatar}
+                  alt={userData?.username || "Uknown user"}
+                />
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
+                <span className="truncate font-medium">
+                  {userData?.username || "Uknown user"}
+                </span>
+                <span className="truncate text-xs">
+                  {userData?.email || "Uknown user"}
+                </span>
               </div>
               <ChevronsUpDownIcon className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -63,12 +75,19 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="size-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
+                  <AvatarImage
+                    src={user.avatar}
+                    alt={userData?.username || "Uknown user"}
+                  />
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                  <span className="truncate font-medium">
+                    {userData?.username || "Uknown user"}
+                  </span>
+                  <span className="truncate text-xs">
+                    {userData?.email || "Uknown user"}
+                  </span>
                 </div>
               </div>
             </DropdownMenuLabel>
