e31e7e14395c0f6c8cbe86e2ac8921d9eae35dde
Xrayya <nur.alkhiyar@gmail.com>
2026-08-12 06:09:02 +0700

    feat(be.db.relation): define tag table relationship

--- backend/src/db/relations.ts
+++ backend/src/db/relations.ts
@@ -7,23 +7,42 @@ export const relations = defineRelations(schema, (r) => ({
       from: r.users.id,
       to: r.notes.authorId,
     }),
+    tags: r.many.tags({
+      from: r.users.id,
+      to: r.tags.ownerId,
+    }),
     refreshTokens: r.many.refreshTokens({
       from: r.users.id,
       to: r.refreshTokens.ownerId,
+    }),
+  },
+  tags: {
+    owner: r.one.users({
+      from: r.tags.ownerId,
+      to: r.users.id,
+      optional: false,
+    }),
+    notes: r.many.notes({
+      from: r.tags.id.through(r.noteTags.tagId),
+      to: r.notes.id.through(r.noteTags.noteId),
     }),
   },
   notes: {
     author: r.one.users({
       from: r.notes.authorId,
       to: r.users.id,
-      optional: false
+      optional: false,
+    }),
+    tags: r.many.tags({
+      from: r.notes.id.through(r.noteTags.noteId),
+      to: r.tags.id.through(r.noteTags.tagId),
     }),
   },
   refreshTokens: {
     owner: r.one.users({
       from: r.refreshTokens.ownerId,
       to: r.users.id,
-      optional: false
+      optional: false,
     }),
   },
 }));
