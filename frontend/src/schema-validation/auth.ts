f218a2a1676c73a970a0676192fc3b75bc1cf8b3
Xrayya <nur.alkhiyar@gmail.com>
2026-08-14 13:44:25 +0700

    fix(fe): forgot to commit updated register schema

--- frontend/src/schema-validation/auth.ts
+++ frontend/src/schema-validation/auth.ts
@@ -1,17 +1,25 @@
 import { z } from "zod";
 
-export const registerSchema = z.object({
-  email: z.email(),
-  username: z.string().min(3),
-  password: z
-    .string()
-    .min(8)
-    .refine((password) => {
-      return (
-        /[a-z]/.test(password) && /[A-Z]/.test(password) && /\d/.test(password)
-      );
-    }, "Password must contain at least one uppercase letter, one lowercase letter, and one number"),
-});
+export const registerSchema = z
+  .object({
+    email: z.email(),
+    username: z.string().min(3),
+    password: z
+      .string()
+      .min(8)
+      .refine((password) => {
+        return (
+          /[a-z]/.test(password) &&
+          /[A-Z]/.test(password) &&
+          /\d/.test(password)
+        );
+      }, "Password must contain at least one uppercase letter, one lowercase letter, and one number"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password !== data.confirmPassword, {
+    error: "Password does not match",
+    path: ["confirmPassword"],
+  });
 
 export const googleCompleteRegisterSchema = z.object({
   username: z.string().min(3),
